89bbcf7d8e53d269bee553e9969cde036088e10e
Jared Griffin <griffinjared1388@gmail.com>
2019-03-07 10:45:12 -0600

    Removed testing outline from hero

--- src/hero/hero.js
+++ src/hero/hero.js
@@ -6,7 +6,7 @@ import Content from '../core/content';
 import data from './data';
 
 const Hero = () => (
-    <div style={{ border: '1px solid blue' }}>
+    <div>
         <Header size={1} style={{ textAlign: 'center', paddingBottom: '64px' }}>{data.title}</Header>
         <Content style={{ paddingBottom: '16px' }}>{data.message}</Content>
         <Content>{data.goals}</Content>
