506cf1e9fb4ddf86cef61a5850a068ecd7312efb
Jared Griffin <griffinjared1388@gmail.com>
2019-03-12 14:53:54 -0500

    Added documents section with filler docs

---
+++ src/documents/data.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        name: 'Resume',
+        src: `${process.env.PUBLIC_URL}/docs/resume.pdf`
+    },
+    {
+        name: 'Cumulative Reflection',
+        src: `${process.env.PUBLIC_URL}/docs/cumulative-reflection.pdf`
+    },
+    {
+        name: 'General Education Reflection',
+        src: `${process.env.PUBLIC_URL}/docs/general-education-reflection.pdf`
+    },
+    {
+        name: 'Ethics Paper',
+        src: `${process.env.PUBLIC_URL}/docs/ethics-paper.pdf`
+    }
+];
--- src/documents/documents.js
+++ src/documents/documents.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
+
+import Header from '../core/header';
+import Content from '../core/content';
+
+import data from './data';
+
+const getDocuments = documents => documents.map(({ name, src }) => (
+    <Content style={{ textDecoration: 'underline' }}>
+        <Link href={src}>
+            {name}
+        </Link>
+    </Content>
+));
 
 const Documents = ({ style }) => (
     <div style={style}>
-        <Typography>This is where the various documents I need will go</Typography>
+        <Header size={5} style={{ paddingBottom: '32px' }}>Documents</Header>
+        <div style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+            {getDocuments(data)}
+        </div>
     </div>
 );
 
