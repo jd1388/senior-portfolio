54f7ba73c55ce0659063848f42ba998dbbf69ee3
Jared Griffin <griffinjared1388@gmail.com>
2019-03-13 19:35:23 -0500

    Added contact info

--- src/contact-info/contact-info.js
+++ src/contact-info/contact-info.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+import Header from '../core/header';
+import Content from '../core/content';
+
+import data from './data';
+
+const getContactInfo = contactMethods => contactMethods.map(({ name, src }) => (
+    <Content style={{ textDecoration: 'underline' }}>
+        <Link href={src} target='_blank'>
+            {name}
+        </Link>
+    </Content>
+));
 
 const ContactInfo = ({ style }) => (
     <div style={style}>
-        <Typography>This is where my contact info will go</Typography>
+        <Header size={5} style={{ paddingBottom: '32px' }}>Contact Info</Header>
+        <div style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+            {getContactInfo(data)}
+        </div>
     </div>
 );
 
